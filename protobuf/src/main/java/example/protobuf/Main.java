785c9c48745704566f791d740885c9e0bd7b90d0
Pojo Zhang <pojo.zhang@gmail.com>
2017-09-25 16:43:31 +0800

    update main

--- protobuf/src/main/java/example/protobuf/Main.java
+++ protobuf/src/main/java/example/protobuf/Main.java
@@ -1,10 +1,22 @@
 package example.protobuf;
 
-import example.protobuf.people;
+import com.google.protobuf.Any;
+import example.protobuf.people.ApiResponseOuterClass.ApiResponse;
+import example.protobuf.people.PeopleOuterClass.People;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        People people=new People();
+    public static void main(String[] args) throws IOException {
+        People people = People.newBuilder().setName("proto").setAge(1).build();
+        ApiResponse response = ApiResponse.newBuilder().setData(Any.pack(people)).build();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        response.writeTo(outputStream);
+
+        ApiResponse r = ApiResponse.parseFrom(outputStream.toByteArray());
+        System.out.println(r.getData().getTypeUrl());
+        System.out.println(r.getData().unpack(People.class).getName());
     }
 }
