67abfa0df00865bddefc1ad0a14bf5e70d9bcd08
Pojo Zhang <pojo.zhang@gmail.com>
2017-09-25 17:41:22 +0800

    add map case

--- protobuf/src/main/java/example/protobuf/Main.java
+++ protobuf/src/main/java/example/protobuf/Main.java
@@ -1,7 +1,7 @@
 package example.protobuf;
 
 import com.google.protobuf.Any;
-import example.protobuf.people.ApiResponseOuterClass.ApiResponse;
+import example.protobuf.people.ResponseOuterClass.Response;
 import example.protobuf.people.PeopleOuterClass.People;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -9,14 +9,18 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        People people = People.newBuilder().setName("proto").setAge(1).build();
-        ApiResponse response = ApiResponse.newBuilder().setData(Any.pack(people)).build();
+        People people = People.newBuilder().setName("proto").setAge(1).setBirthDate(1000000).build();
+        Response response = Response.newBuilder().setData(Any.pack(people)).putHeaders("Content-Type", "pb").build();
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         response.writeTo(outputStream);
 
-        ApiResponse r = ApiResponse.parseFrom(outputStream.toByteArray());
+        Response r = Response.parseFrom(outputStream.toByteArray());
+        System.out.println(r.getHeadersMap());
+        People p = r.getData().unpack(People.class);
         System.out.println(r.getData().getTypeUrl());
-        System.out.println(r.getData().unpack(People.class).getName());
+        System.out.println(p.getName());
+        System.out.println(p.getAge());
+        System.out.println(p.getBirthDate());
     }
 }
